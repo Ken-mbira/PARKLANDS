e819ed69e9b9883eb8b525557fb87c860e0fe76f
ken <ken.mbira@student.moringaschool.com>
2021-11-04 09:07:26 +0300

    bind to the locations on request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,7 +44,8 @@ import {MatSelectModule} from '@angular/material/select';
     HttpClientModule,
     MatSnackBarModule,
     FormsModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-lot/new-lot.component.ts
+++ src/app/new-lot/new-lot.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AccountService } from '../account.service';
+import { Parking } from '../parking';
+
 @Component({
   selector: 'app-new-lot',
   templateUrl: './new-lot.component.html',
   styleUrls: ['./new-lot.component.css']
 })
 export class NewLotComponent implements OnInit {
+  parking = new Parking("","","","","","","")
   locations: any;
 
   constructor(private accountService:AccountService) { }
