145733c4bc58812fde80ca0fed820e0365a1cde5
ken <ken.mbira@student.moringaschool.com>
2021-11-04 13:35:35 +0300

    aupdate environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL:"http://127.0.0.1:8000/"
+  BASE_URL:"http://127.0.0.1:8000/",
+  CLOUDINARY_URL:'https://res.cloudinary.com/duykqvfcr/'
 };
