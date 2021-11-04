6ac1bf411ae3f9236d937cfec11d3a89807a1f59
ken <ken.mbira@student.moringaschool.com>
2021-11-04 07:55:02 +0300

    set up images from cloudinary

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  
+  cloudinaryUrl = environment.CLOUDINARY_URL
+  user = new User("","","","","")
 
-  constructor() { }
+
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.userService.currentUser.subscribe(user => this.user = user)
+    console.log(this.user)
   }
 
 }
